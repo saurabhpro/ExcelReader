1e7dae8b5d15a721801d56a31e4fe52824d2f0de
Saurabh Kumar <saurabhk1511@gmail.com>
2016-02-03 10:44:01 +0530

    Initial commit

---
+++ src/ReadExcelDemo.java
@@ -0,0 +1,56 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+/**
+ * Created by kumars on 2/3/2016.
+ */
+//import statements
+public class ReadExcelDemo
+{
+
+    public static void main(String... args)
+    {
+        try
+        {
+            FileInputStream file = new FileInputStream(new File("MyExcelPOI.xlsx"));
+
+            //Create Workbook instance holding reference to .xlsx file
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            //Get first/desired sheet from the workbook
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            //Iterate through each rows one by one
+            for (Row row : sheet) {
+                //For each row, iterate through all the columns
+                Iterator<Cell> cellIterator = row.cellIterator();
+
+                //iterator
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    //Check the cell type and format accordingly
+                    switch (cell.getCellType()) {
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                    }
+                }
+                System.out.println("");
+            }
+            file.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
