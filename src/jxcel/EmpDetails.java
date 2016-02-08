bf09fa666b47b3163be8105faa45d6d1263eaa5a
Saurabh Kumar <saurabhk1511@gmail.com>
2016-02-08 17:21:44 +0530

    jexcel files created Updated YAAY Time calculation

--- src/jxcel/EmpDetails.java
+++ src/jxcel/EmpDetails.java
@@ -7,12 +7,12 @@ package jxcel;
 public class EmpDetails {
     String name;
     String empID;
-    String checkIn;
-    String checkOut;
-    String overTime;
-    String status;
+    String[] checkIn;
+    String[] checkOut;
+    String[] overTime;
+    String[] status;
 
-    EmpDetails(String ename, String eID, String chkIn, String chkOut, String ovrTime, String estatus) {
+    EmpDetails(String ename, String eID, String[] chkIn, String[] chkOut, String[] ovrTime, String[] estatus) {
         name = ename;
         empID = eID;
         checkIn = chkIn;
--- src/jxcel/ReadExcel.java
+++ src/jxcel/ReadExcel.java
@@ -1,13 +1,15 @@
 package jxcel;
 
 import jxl.Cell;
-import jxl.CellType;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.StringTokenizer;
 
 /**
@@ -40,45 +42,61 @@ public class ReadExcel {
 
             EmpDetails[] emp = new EmpDetails[91];
             Cell cell;
-            String[] details = new String[6];
+            String[][] details = new String[6][];
             String empTime;
 
             int addrow = 0;
             for (int i = 0; i < 91; i++) {
                 cell = sheet.getCell(3, 13 + (18 * addrow));
-                details[0] = cell.getContents();
-                cell = sheet.getCell(3, 15 + (18 * addrow));
-                details[1] = cell.getContents();
-                cell = sheet.getCell(3, 20 + (18 * addrow));
-                empTime = cell.getContents();
-                String temp;
-                StringTokenizer st = new StringTokenizer(empTime, "   ");
-                for (int j = 2; j < 6; j++) {
-                    if (st.hasMoreElements()) {
-                        temp = (String) st.nextElement();
-                        if (temp.equals("A")||temp.equals("P")||temp.equals("P/A")||temp.equals("W"))
-                            details[5] = temp;
-                        else
-                            details[j] = temp;
+                details[0] = new String[1];
+                details[0][0] = cell.getContents();
 
+                cell = sheet.getCell(3, 15 + (18 * addrow));
+                details[1] = new String[1];
+                details[1][0] = cell.getContents();
+
+
+                String temp;
+                details[2] = new String[31];
+                details[3] = new String[31];
+                details[4] = new String[31];
+                details[5] = new String[31];
+
+
+                for (int k = 0; k < 31; k++) {
+                    cell = sheet.getCell(k, 20 + (18 * addrow));
+                    empTime = cell.getContents();
+                    StringTokenizer st = new StringTokenizer(empTime, "   ");
+
+                    for (int j = 2; j < 6; j++) {
+                        if (st.hasMoreElements()) {
+                            temp = (String) st.nextElement();
+                            if (temp.equals("A") || temp.equals("P") || temp.equals("P/A") || temp.equals("W"))
+                                details[5][k] = temp;
+                            else
+                                details[j][k] = temp;
+
+                        }
                     }
 
 
                 }
-                emp[i] = new EmpDetails(details[0], details[1], details[2], details[3], details[4], details[5]);
-
-               details[2]= details[3] = details[4] = details[5] = null;
+                emp[i] = new EmpDetails(details[0][0], details[1][0], details[2], details[3], details[4], details[5]);
                 addrow++;
             }
             for (int i = 0; i < 91; i++) {
                 System.out.println("Name: " + emp[i].name);
                 System.out.println("Employee ID: " + emp[i].empID);
-                System.out.println("Check In Time: " + emp[i].checkIn);
-                System.out.println("Check Out Time: " + emp[i].checkOut);
 
-                System.out.println("Status: " + emp[i].status + "\n");
+                for (int j = 0; j < 31; j++) {
+                    calculateTimeDifference(emp[i].checkIn[j], emp[i].checkOut[j]);
+
+                    System.out.print((j + 1) + " In Time: " + emp[i].checkIn[j]);
+                    System.out.print("\tOut Time: " + emp[i].checkOut[j]);
+                    System.out.print("\tStatus: " + emp[i].status[j] + "\n");
+                }
+                System.out.println();
             }
-
 
 
             /*for (int col = 1; col <=sheet.getColumns(); col++)
@@ -110,4 +128,29 @@ public class ReadExcel {
         }
     }
 
+    private void calculateTimeDifference(String s, String s1) {
+        try {
+
+            if (s != null && s1 != null) {
+                System.out.println("xxxxTime" );
+                SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm");
+                Date parseDate = dateFormat.parse(s);
+                Time time = new Time(parseDate.getTime());
+              //  System.out.println(ppstime);
+
+                long inTime = time.getTime();
+                parseDate = dateFormat.parse(s1);
+                time = new Time(parseDate.getTime());
+                long outTime = time.getTime();
+
+                System.out.println("xxxxTime" + (outTime - inTime)/3600);
+            }
+        } catch (Exception e) {//this generic but you can control another types of exception
+
+
+        }
+
+
+    }
 }
+
