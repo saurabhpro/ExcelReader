c342fd03aed234dfe961062e631bba8923122970
AroraA <amrita.arora.1192@gmail.com>
2016-02-09 17:20:39 +0530

    jexcel files created Updated YAAY Time calculation Hrnet reader

--- src/jxcel/ReadExcel.java
+++ src/jxcel/ReadExcel.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import java.util.Date;
 import java.util.Objects;
 import java.util.StringTokenizer;
 
@@ -63,7 +64,15 @@ public class ReadExcel {
                 cell = sheet.getRow(i).getCell(j);
                 switch (cell.getCellType()) {
                     case Cell.CELL_TYPE_NUMERIC:
+                        int temp = (int) cell.getNumericCellValue();
+                        /*Date dt = new Date(temp);
+
+                        if(temp >40000){
+                            //details[j] = getJavaDate();
+                        }
+                        */
                         details[j] = Objects.toString(cell.getNumericCellValue());
+
                         break;
                     case Cell.CELL_TYPE_STRING:
                         details[j] = cell.getStringCellValue();
@@ -77,11 +86,11 @@ public class ReadExcel {
 
         for (int i = 0; i < numberOfRowsInHr - 1; i++) {
             System.out.print(hrnetDetails[i].hrID);
-            System.out.print(" " + hrnetDetails[i].name);
-            System.out.print(" " + hrnetDetails[i].requestID);
-            System.out.print(" " + hrnetDetails[i].leaveType);
-            System.out.print(" " + hrnetDetails[i].startDate + " " + hrnetDetails[i].endDate);
-            System.out.println(" " + hrnetDetails[i].absenceTime);
+            System.out.print("\t" + hrnetDetails[i].name);
+            System.out.print("\t" + hrnetDetails[i].requestID);
+            System.out.print("\t" + hrnetDetails[i].leaveType);
+            System.out.print("\t" + hrnetDetails[i].startDate + "\t" + hrnetDetails[i].endDate);
+            System.out.println("\t" + hrnetDetails[i].absenceTime);
 
         }
         file.close();
